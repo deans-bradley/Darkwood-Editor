725ced38caf2bc5d1ae5f67cca3710a9309dbfcb
deans-bradley <bradleydeansdev@gmail.com>
2024-04-16 15:54:06 +0200

    Add DarkwoodEditorWPF

---
+++ DarkwoodEditorWPF/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+using DarkwoodEditorWPF.Models;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace DarkwoodEditorWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string? filePath;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileDialog()
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "DAT Files (*.dat)|*.dat|All Files (*.*)|*.*";
+            openFile.InitialDirectory = $@"C:\Users\{Environment.UserName}\AppData\LocalLow\Acid Wizard Studio\Darkwood";
+
+            openFile.ShowDialog();
+
+            if (openFile.CheckFileExists && openFile.FileName != String.Empty)
+            {
+                filePath = openFile.FileName;
+                Root root = DeserializeJson();
+            }
+            else if (openFile.CheckFileExists == false)
+            {
+                MessageBox.Show("File does not exist.");
+            }
+        }
+
+        public Root DeserializeJson()
+        {
+            string json = File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<Root>(json);
+        }
+    }
+}
--- DarkwoodEditorWPF/obj/Debug/net8.0-windows/DarkwoodEditorWPF_ir5u3b0p_wpftmp.AssemblyInfo.cs
+++ DarkwoodEditorWPF/obj/Debug/net8.0-windows/DarkwoodEditorWPF_ir5u3b0p_wpftmp.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("DarkwoodEditor")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("DarkwoodEditorWPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c64b243df81af70e127a4aff6290c59f523d6e4")]
-[assembly: System.Reflection.AssemblyProductAttribute("DarkwoodEditor")]
-[assembly: System.Reflection.AssemblyTitleAttribute("DarkwoodEditor")]
+[assembly: System.Reflection.AssemblyProductAttribute("DarkwoodEditorWPF")]
+[assembly: System.Reflection.AssemblyTitleAttribute("DarkwoodEditorWPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
