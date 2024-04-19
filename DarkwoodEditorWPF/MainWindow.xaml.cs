871d66456b65489db38e556d7ef7294edaf7ebcd
deans-bradley <bradleydeansdev@gmail.com>
2024-04-19 15:40:22 +0200

    Update MenuItem shortcuts

--- DarkwoodEditorWPF/MainWindow.xaml.cs
+++ DarkwoodEditorWPF/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using DarkwoodEditorWPF.Models;
 using Newtonsoft.Json;
 using System.IO;
 using DarkwoodEditorWPF.ViewModels;
+using System.Windows.Input;
 
 namespace DarkwoodEditorWPF
 {
@@ -32,6 +33,7 @@ namespace DarkwoodEditorWPF
                 filePath = openFile.FileName;
                 Root root = DeserializeJson();
 
+                MainContent.Navigate(new Uri("Views/StartPage.xaml", UriKind.Relative));
                 AddData(root, root.pS);
             }
             else if (openFile.CheckFileExists == false)
@@ -76,9 +78,64 @@ namespace DarkwoodEditorWPF
             DataContext = psViewModel;
         }
 
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        //
+        // File Menu Items
+        //
+        private void OpenCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
         {
             openFileDialog();
         }
+
+        private void SaveCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Save");
+        }
+
+        private void CloseCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Close");
+        }
+
+        private void exitMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+        private void ExitCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+
+        //
+        // Edit Menu Items
+        //
+
+        private void playerStatsMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Navigate(new Uri("Views/StartPage.xaml", UriKind.Relative));
+        }
+
+        private void playerMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Navigate(new Uri("Views/PlayerPage.xaml", UriKind.Relative));
+        }
+
+        private void worldMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void stroyMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void timeAndWeatherMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //
+        // Help Menu Items
+        //
     }
 }
